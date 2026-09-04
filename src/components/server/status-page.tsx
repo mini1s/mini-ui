8795a2e63b2e077d6958185b92d4791ba2e82d4c
Jack Kirkham <jack@minisquare.com>
2026-09-04 17:05:25 +0100

    fix: title and semititle bug on status page, tailwind classes not being recognised

--- src/components/server/status-page.tsx
+++ src/components/server/status-page.tsx
@@ -10,8 +10,8 @@ function mapIcon(name?: IconName) {
 
 export function StatusPage({
     Link,
-    semititle,
-    title,
+    semititle = "Error 404",
+    title = "Page not found",
     text,
     iconClassName,
     iconName,
@@ -26,23 +26,40 @@ export function StatusPage({
     links?: { text: string; to: string; color?: string }[]
 }) {
     return (
-        <div className="min-h-[50vh] flex-1 flex items-center justify-center p-16 gap-16 text-gray-800">
-            <div className={iconClassName ?? `text-gray-400`}>
-                <FontAwesomeIcon icon={mapIcon(iconName)} className="w-32" />
-            </div>
-            <div className="flex flex-col gap-4">
-                {semititle && <h2 className="text-2xl font-space-grotesk font-bold text-gray-600">Error 404</h2>}
-                {title && <h1 className="text-5xl font-space-grotesk font-bold">Page not found</h1>}
-                {text && <p className="font-rubik mt-6">{text}</p>}
-                {links && (
-                    <div className="flex gap-4 mt-6">
-                        {links.map((link, i) => (
-                            <Link key={i} href={link.to} className={`button ${link.color ?? "gray"} font-rubik`}>
-                                {link.text}
-                            </Link>
-                        ))}
-                    </div>
-                )}
+        <div className="min-h-[50vh] flex-1 flex flex-col items-center justify-center p-16 ">
+            <div className="flex items-center justify-center gap-16 text-gray-80 max-w-5xl">
+                <div className={iconClassName ?? `text-gray-400`}>
+                    <FontAwesomeIcon
+                        icon={mapIcon(iconName)}
+                        className="w-32"
+                    />
+                </div>
+                <div className="flex flex-col gap-4">
+                    {semititle && (
+                        <h2 className="text-2xl font-space-grotesk font-bold text-gray-600">
+                            {semititle}
+                        </h2>
+                    )}
+                    {title && (
+                        <h1 className="text-5xl font-space-grotesk font-bold">
+                            {title}
+                        </h1>
+                    )}
+                    {text && <p className="font-rubik mt-6">{text}</p>}
+                    {links && (
+                        <div className="flex gap-4 mt-6">
+                            {links.map((link, i) => (
+                                <Link
+                                    key={i}
+                                    href={link.to}
+                                    className={`button ${link.color ?? "gray"} font-rubik`}
+                                >
+                                    {link.text}
+                                </Link>
+                            ))}
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     )
